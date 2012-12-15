fd7a2e48702980f47f859f2a53a18f9f9190d84d
Max Bogue <max@maxbogue.com>
2012-12-15 14:57:01 -0500

    Finish sequential version of hw1-1.

--- hw1/hw1-1/hw1q1seq.java
+++ hw1/hw1-1/hw1q1seq.java
@@ -1,6 +1,33 @@
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 
-class hw1q1seq {
+public class hw1q1seq {
+
+    private static DecimalFormat df = new DecimalFormat("#.##");
+
+    public static class INS implements Comparable<INS> {
+        public final int student;
+        public final double ins;
+        public INS(int student, double ins) {
+            this.student = student;
+            this.ins = ins;
+        }
+        public int compareTo(INS o) {
+            if (ins < o.ins) {
+                return -1;
+            } else if (ins == o.ins) {
+                return 0;
+            } else {
+                return 1;
+            }
+        }
+        public String toString() {
+            return "#" + student + ": " + df.format(ins);
+        }
+    }
 
     public static double[][] randomMatrix(int m, int n, long seed) {
         Random random = new Random(seed);
@@ -13,6 +40,14 @@ class hw1q1seq {
         return matrix;
     }
 
+    public static double calculateINS(double[] grades) {
+        double total = 0.0;
+        for (int i = 0; i < grades.length; i++) {
+            total += grades[i] * grades[i];
+        }
+        return total / grades.length;
+    }
+
     public static void main (String [] args) {
         if (args.length < 3) {
             System.out.println("Usage: java hw1q1seq m n seed");
@@ -21,6 +56,33 @@ class hw1q1seq {
         int m = Integer.parseInt(args[0]);
         int n = Integer.parseInt(args[1]);
         long seed = Long.parseLong(args[2]);
-        double[][] grades = randomMatrix(m, n, seed);
+        double[][] studentGrades = randomMatrix(m, n, seed);
+        List<INS> inss = new ArrayList<INS>(m);
+        for (int i = 0; i < m; i++) {
+            inss.add(i, new INS(i, calculateINS(studentGrades[i])));
+        }
+        List<INS> sortedINSs = new ArrayList<INS>(inss);
+        Collections.sort(sortedINSs, Collections.reverseOrder());
+        System.out.println(sortedINSs);
+        double[] fnss = new double[m];
+        for (int i = 0; i < m; i++) {
+            double fns;
+            if (i < m * 0.1) {
+                fns = 4.0;
+            } else if (i < m * 0.3) {
+                fns = 3.0;
+            } else if (i < m * 0.6) {
+                fns = 2.0;
+            } else if (i < m * 0.8) {
+                fns = 1.0;
+            } else {
+                fns = 0.0;
+            }
+            fnss[sortedINSs.get(i).student] = fns;
+        }
+        for (int i = 0; i < m; i++) {
+            System.out.println("#" + (i + 1) + ": INS=" + df.format(inss.get(i).ins)
+                + ", FNS=" + fnss[i]);
+        }
     }
 }
