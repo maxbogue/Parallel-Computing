6e4c0aed092696185672d4875443d2d7f7b64325
Max Bogue <max@maxbogue.com>
2013-02-03 15:43:53 -0500

    First work on matrix multiplication, but wrong approach for the data storage.

---
+++ hw3/hw3-4/hw3q4clu.java
@@ -0,0 +1,93 @@
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+
+import edu.rit.mp.IntegerBuf;
+import edu.rit.mp.buf.IntegerMatrixBuf;
+import edu.rit.pj.Comm;
+import edu.rit.util.Arrays;
+import edu.rit.util.Random;
+import edu.rit.util.Range;
+
+/**
+ * Class for cluster version of shifting an image.
+ */
+public class hw3q4clu {
+
+    public static void main(String[] args) throws Exception {
+        Comm.init(args);
+        Comm world = Comm.world();
+        int size = world.size();
+        int rank = world.rank();
+
+        // Process args.
+        if (args.length != 4) {
+            System.out.println("Usage: java hw3q4clu n seed outputFile");
+            System.exit(1);
+        }
+        final int N = Integer.parseInt(args[0]);
+        long seed = Long.parseLong(args[1]);
+        String outFile = args[2];
+
+        int p = (int)Math.sqrt(size);
+        int n = N / p;
+        int ri = rank / p;
+        int ci = rank % p;
+        int[][] a = new int[n][n];
+        int[][] b = new int[n][n];
+        int[][] c = new int[n][n];
+        int[][] row = new int[n][n * p];
+        int[][] col = new int[n * p][n];
+
+        // Set up ranges!
+        Range[] ranges = new Range(0, n - 1).subranges(p);
+        //Range range = ranges[rank];
+        //int lb = range.lb();
+        //int ub = range.ub();
+
+        // And buffers to hold them!
+        IntegerBuf[] rowSlices = IntegerBuf.colSliceBuffers(row, ranges);
+        IntegerBuf[] colSlices = IntegerBuf.rowSliceBuffers(col, ranges);
+
+        // Start timing.
+        long t1 = System.currentTimeMillis();
+
+        // Generate numbers.
+        Random random = Random.getInstance(seed);
+        // Jump to the start of this patch of A and generate nums.
+        random.skip(n * n * (ri * p + ci));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i][j] = random.nextInt(100);
+            }
+            // Skip to the next row of this patch.
+            random.skip(N - n);
+        }
+        // Skip to the start of B.
+        random.setSeed(seed);
+        random.skip(N * N);
+        // Jump to the start of this patch of B and generate nums.
+        random.skip(n * n * (ri * p + ci));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                b[i][j] = random.nextInt(100);
+            }
+            // Skip to the next row of this patch.
+            random.skip(N - n);
+        }
+
+        // Use the row index and col index as tags and gather the data.
+        world.allGather(ri, IntegerBuf.buffer(a), rowSlices);
+        world.allGather(ci, IntegerBuf.buffer(b), colSlices);
+
+        // Write output.
+        if (rank == 0) {
+        }
+
+        // Stop timing.
+        long t2 = System.currentTimeMillis();
+        if (rank == 0) {
+            System.out.println((t2-t1) + " ms");
+        }
+    }
+
+}
