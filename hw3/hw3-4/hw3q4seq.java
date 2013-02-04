c5016c0e5068c8cf7e2ddcfd65a2bdbaf2ee9b0c
Max Bogue <max@maxbogue.com>
2013-02-03 19:00:07 -0500

    Strip down to sequential version.

--- hw3/hw3-4/hw3q4seq.java
+++ hw3/hw3-4/hw3q4seq.java
@@ -1,16 +1,12 @@
 import java.io.PrintWriter;
 
-import edu.rit.mp.IntegerBuf;
-import edu.rit.mp.buf.IntegerMatrixBuf;
 import edu.rit.pj.Comm;
-import edu.rit.util.Arrays;
 import edu.rit.util.Random;
-import edu.rit.util.Range;
 
 /**
  * Class for cluster version of shifting an image.
  */
-public class hw3q4clu {
+public class hw3q4seq {
 
     public static void main(String[] args) throws Exception {
         Comm.init(args);
@@ -31,82 +27,43 @@ public class hw3q4clu {
         final int n = N / p;
         final int ri = rank / p;
         final int ci = rank % p;
-        int[][] a = new int[n][n];
-        int[][] b = new int[n][n];
-        int[][] c = new int[n][n];
-        int[][] rows = new int[n][N];
-        int[][] cols = new int[N][n];
-        Range[] ranges = new Range(0, N - 1).subranges(p);
+        int[][] A = new int[N][N];
+        int[][] B = new int[N][N];
+        int[][] C = new int[N][N];
 
         // Start timing.
         long t1 = System.currentTimeMillis();
 
         // Generate numbers.
         Random random = Random.getInstance(seed);
-        // Jump to the start of this patch of A and generate nums.
-        random.skip(n * n * (ri * p + ci));
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                a[i][j] = random.nextInt(100);
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                A[i][j] = random.nextInt(100);
             }
-            // Skip to the next row of this patch.
-            random.skip(N - n);
         }
-        // Skip to the start of B.
-        random.setSeed(seed);
-        random.skip(N * N);
-        // Jump to the start of this patch of B and generate nums.
-        random.skip(n * n * (ri * p + ci));
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                b[i][j] = random.nextInt(100);
-            }
-            // Skip to the next row of this patch.
-            random.skip(N - n);
-        }
-
-        // And buffers to hold them!
-        IntegerBuf[] rowSlices = IntegerBuf.colSliceBuffers(rows, ranges);
-        IntegerBuf[] colSlices = IntegerBuf.rowSliceBuffers(cols, ranges);
-
-        Comm rowComm = null, colComm = null;
-        for (int i = 0; i < p; i++) {
-            if (i == ri) {
-                rowComm = world.createComm(true);
-            } else {
-                world.createComm(false);
-            }
-            if (i == ci) {
-                colComm = world.createComm(true);
-            } else {
-                world.createComm(false);
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                B[i][j] = random.nextInt(100);
             }
         }
 
-        // Use the row index and col index as tags and gather the data.
-        rowComm.allGather(IntegerBuf.buffer(a), rowSlices);
-        colComm.allGather(IntegerBuf.buffer(b), colSlices);
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        // Do the matrix multiplication.
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
                 for (int k = 0; k < N; k++) {
-                    c[i][j] += rows[i][k] * cols[k][j];
+                    C[i][j] += A[i][k] * B[k][j];
                 }
             }
         }
 
+        // Stop timing.
+        long t2 = System.currentTimeMillis();
+        System.out.println((t2-t1) + " ms");
+
         // Write output.
-        if (rank == 0) {
-            int[][] C = new int[N][N];
-            world.gather(0, IntegerBuf.buffer(c), IntegerBuf.patchBuffers(C, ranges, ranges));
-            long t2 = System.currentTimeMillis();
-            System.out.println((t2-t1) + " ms");
-            PrintWriter out = new PrintWriter(outFileName);
-            printMatrix(C, out);
-            out.close();
-        } else {
-            world.gather(0, IntegerBuf.buffer(c), null);
-        }
+        PrintWriter out = new PrintWriter(outFileName);
+        printMatrix(C, out);
+        out.close();
 
     }
 
