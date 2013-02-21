f0a38ee086f6b90f7356938953590e1fd4816de5
Max Bogue <max@maxbogue.com>
2013-02-20 19:10:06 -0500

    Convert sequential version to match new parallel paradigms.

--- hw4/hw4-3/State.java
+++ hw4/hw4-3/State.java
@@ -2,55 +2,12 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
-public class State {
-
-    public static int[] capacities = new int[0];
+public class State implements java.io.Serializable {
 
     public final int[] buckets;
-    public final State prev;
-
-    public State(final int[] buckets, State prev) {
-        this.buckets = buckets;
-        this.prev = prev;
-    }
 
     public State(final int[] buckets) {
-        this(buckets, null);
-    }
-
-    public List<State> nextState() {
-        int[] b;
-        List<State> next = new LinkedList<State>();
-        for (int i = 0; i < buckets.length; i++) {
-
-            // Fill i.
-            if (buckets[i] != capacities[i]) {
-                b = (int[])buckets.clone();
-                b[i] = capacities[i];
-                next.add(new State(b, this));
-            }
-
-            // Empty i.
-            if (buckets[i] != 0) {
-                b = (int[])buckets.clone();
-                b[i] = 0;
-                next.add(new State(b, this));
-            }
-
-            // Pour from i to j.
-            for (int j = 0; j < buckets.length; j++) {
-                if (i == j) continue;
-                if (buckets[j] != capacities[j] && buckets[i] != 0) {
-                    int v = Math.min(capacities[j] - buckets[j], buckets[i]);
-                    b = (int[])buckets.clone();
-                    b[i] -= v;
-                    b[j] += v;
-                    next.add(new State(b, this));
-                }
-            }
-
-        }
-        return next;       
+        this.buckets = buckets;
     }
 
     public boolean hasBucket(int amount) {
@@ -60,13 +17,6 @@ public class State {
             }
         }
         return false;
-    }
-
-    public void printSteps() {
-        if (prev != null) {
-            prev.printSteps();
-        }
-        System.out.println(this);
     }
 
     public int hashCode() {
--- hw4/hw4-3/hw4q3seq.java
+++ hw4/hw4-3/hw4q3seq.java
@@ -1,11 +1,61 @@
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Queue;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import edu.rit.pj.Comm;
 
 public class hw4q3seq {
+
+    private static Map<State,State> prevs = new HashMap<State,State>();
+    private static int[] capacities;
+
+    private static void putInListChecked(List<State> ls, int[] b, State prev) {
+        State newState = new State(b);
+        if (!prevs.containsKey(newState)) {
+            ls.add(newState);
+            prevs.put(newState, prev);
+        }
+    }
+
+    private static List<State> nextStates(State s) {
+        int[] buckets = s.buckets;
+        int[] b;
+        List<State> next = new LinkedList<State>();
+        for (int i = 0; i < buckets.length; i++) {
+
+            // Fill i.
+            if (buckets[i] != capacities[i]) {
+                b = (int[])buckets.clone();
+                b[i] = capacities[i];
+                putInListChecked(next, b, s);
+            }
+
+            // Empty i.
+            if (buckets[i] != 0) {
+                b = (int[])buckets.clone();
+                b[i] = 0;
+                putInListChecked(next, b, s);
+            }
+
+            // Pour from i to j.
+            for (int j = 0; j < buckets.length; j++) {
+                if (i == j) continue;
+                if (buckets[j] != capacities[j] && buckets[i] != 0) {
+                    int v = Math.min(capacities[j] - buckets[j], buckets[i]);
+                    b = (int[])buckets.clone();
+                    b[i] -= v;
+                    b[j] += v;
+                    putInListChecked(next, b, s);
+                }
+            }
+
+        }
+        return next;       
+    }
 
     public static void main(String[] args) throws Exception {
         Comm.init(args);
@@ -20,37 +70,33 @@ public class hw4q3seq {
         // The number of buckets.
         int k = args.length - 1;
         // The capacity of each bucket.
-        int[] capacities = new int[k];
+        capacities = new int[k];
         for (int i = 0; i < k; i++) {
             capacities[i] = Integer.parseInt(args[i + 1]);
         }
-        State.capacities = capacities;
-
-        // The initial state of each bucket.
-        final int[] emptyBuckets = new int[k];
 
         // Start timing.
         long t1 = System.currentTimeMillis();
 
-        Set<State> seen = new HashSet<State>();
+        State initial = new State(new int[k]);
+        prevs.put(initial, null);
+
         Queue<State> queue = new LinkedList<State>();
-        queue.add(new State(emptyBuckets));
+        queue.add(initial);
 
         State s = null;
         while (!queue.isEmpty()) {
             s = queue.remove();
-            if (seen.contains(s)) continue;
             if (s.hasBucket(target)) break;
-            seen.add(s);
-            queue.addAll(s.nextState());
+            queue.addAll(nextStates(s));
         }
 
         // Stop timing.
         long t2 = System.currentTimeMillis();
 
         // Print the result.
-        if (s.hasBucket(target)) {
-            s.printSteps();
+        if (s != null && s.hasBucket(target)) {
+            printSteps(s);
         } else {
             System.out.println("No solution found.");
         }
@@ -58,6 +104,14 @@ public class hw4q3seq {
         // Print time results.
         System.err.println((t2-t1) + " ms");
 
+    }
+
+    private static void printSteps(State s) {
+        State p = prevs.get(s);
+        if (p != null) {
+            printSteps(p);
+        }
+        System.out.println(s);
     }
 
 }
